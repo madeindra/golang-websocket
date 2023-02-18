ce2c6bfa096e67d75d2f7fcc7d2cc56336242aec
Made Indra <made.indra@protonmail.com>
2023-02-18 09:23:25 +0700

    add ping to prevent dangling connection

--- internal/websocket/handler.go
+++ internal/websocket/handler.go
@@ -3,9 +3,19 @@ package websocket
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
+)
+
+const (
+	// time to read the next client's pong message
+	pongWait = 60 * time.Second
+	// time period to send pings to client
+	pingPeriod = (pongWait * 9) / 10
+	// time allowed to write a message to client
+	writeWait = 10 * time.Second
 )
 
 var upgrader = websocket.Upgrader{
@@ -37,13 +47,62 @@ func HandleWS(w http.ResponseWriter, r *http.Request) {
 	// greet the new client
 	server.Send(conn, fmt.Sprintf("Server: Welcome! Your ID is %s", clientID))
 
+	// create channel to signal client health
+	done := make(chan struct{})
+
+	go writePump(conn, clientID, done)
+	readPump(conn, clientID, done)
+}
+
+// readPump process incoming messages and set the settings
+func readPump(conn *websocket.Conn, clientID string, done chan<- struct{}) {
+	// set limit, deadline to read & pong handler
+	conn.SetReadLimit(512)
+	conn.SetReadDeadline(time.Now().Add(pongWait))
+	conn.SetPongHandler(func(string) error {
+		conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+
 	// message handling
 	for {
+		// read incoming message
 		_, msg, err := conn.ReadMessage()
+		// if error occured
 		if err != nil {
+			// remove from the client
 			server.RemoveClient(clientID)
+			// set health status to unhealthy by closing channel
+			close(done)
+			// stop process
+			break
+		}
+
+		// if no error, process incoming message
+		server.ProcessMessage(conn, clientID, msg)
+	}
+}
+
+// writePump sends ping to the client
+func writePump(conn *websocket.Conn, clientID string, done <-chan struct{}) {
+	// create ping ticker
+	ticker := time.NewTicker(pingPeriod)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			// send ping message
+			err := conn.WriteControl(websocket.PingMessage, []byte{}, time.Now().Add(writeWait))
+			if err != nil {
+				// if error sending ping, remove this client from the server
+				server.RemoveClient(clientID)
+				// stop sending ping
+				return
+			}
+		case <-done:
+			// if process is done, stop sending ping
 			return
 		}
-		server.ProcessMessage(conn, clientID, msg)
 	}
 }
