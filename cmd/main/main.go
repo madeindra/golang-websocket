1ade955aa31bfa50ff65ede47219889f91a09494
Made Indra <made.indra@protonmail.com>
2023-02-19 18:05:04 +0700

    use `net/http`

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,16 +3,13 @@ package main
 import (
 	"net/http"
 
-	"github.com/gorilla/mux"
-
 	"github.com/madeindra/golang-websocket/internal/websocket"
 )
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/socket", websocket.HandleWS)
+	http.HandleFunc("/socket", websocket.HandleWS)
 
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
 }
